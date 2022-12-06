33e471f8735c40625bf56e6721b36b211b323301
AndrewLester <alester220@gmail.com>
2022-12-06 14:02:20 -0600

    Fix

--- 6/6.go
+++ 6/6.go
@@ -4,29 +4,6 @@ import (
 	"aoc2022/input"
 )
 
-type Stack []string
-
-// func firstStartOfX(text string, xLen int) int {
-// 	buffer := map[string]int{}
-// 	for i, c := range text {
-// 		if _, ok := buffer[string(c)]; !ok && len(buffer) == xLen-1 {
-// 			return i
-// 		}
-
-// 		if i > xLen-1 {
-// 			oldChar := string(text[i-(xLen-1)])
-// 			buffer[oldChar]--
-// 			val := buffer[oldChar]
-// 			if val == 0 {
-// 				delete(buffer, oldChar)
-// 			}
-// 		}
-// 		buffer[string(c)]++
-// 	}
-
-// 	return -1
-// }
-
 func firstStartOfX(text string, xLen int) int {
 	buffer := map[string]int{}
 
